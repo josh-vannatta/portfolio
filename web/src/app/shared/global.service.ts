47cb421c1ca4bb9fc8bd98555ed2510695e4d5c4
Josh Van Natta <jjvannatta88@gmail.com>
2018-05-03 02:36:22 -0600

    Resized the admin login page for mobile / tablet

--- web/src/app/shared/global.service.ts
+++ web/src/app/shared/global.service.ts
@@ -5,14 +5,14 @@ import { HttpHeaders } from '@angular/common/http';
 export class GlobalService {
   appName: string = 'JVN Portfolio';
   appAuthor: string = 'Joshua Van Natta';
-  // serverUrl: string = 'http://127.0.0.1:8000/';
-  serverUrl: string = '/';
+  // serverUrl: string = 'http://127.0.0.1:8000/'; // local
+  serverUrl: string = '/'; // web
   requestHeaders: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
   private passport =  {
         grant_type: "password",
         client_id: 2,
-        // client_secret: "cTNF86dUt77vWbGcWgLqboHHBYZvhTDZ05l34phB",
-        client_secret: "LtJRGo9qcJS7KHK2wq1wXz8vkdrA1QtPGbZX4v9f",
+        // client_secret: "cTNF86dUt77vWbGcWgLqboHHBYZvhTDZ05l34phB", // local
+        client_secret: "LtJRGo9qcJS7KHK2wq1wXz8vkdrA1QtPGbZX4v9f", // web
         username: null,
         password: null,
         scope: "*"
@@ -24,5 +24,21 @@ export class GlobalService {
 
     return this.passport;
   }
+
+  /*
+
+  The problem at the moment is that I need content for this section of my site and I need it finished today.
+  I don't know what employers would like / not like and my intuition is that it should be genuine and infornmative.
+  How do I make these descriptions informative and geniune? How do I write something worth reading that is about my experience and ability?
+
+  EXPERIENCE & ABILITY is the focus
+
+  I have experience building / doing ____ with ____.
+  I am interested in ____ using ____.
+  I have used ____ libraries / plugins for ____.
+
+  How can I manipulate my employers to get what I want?
+
+  */
 
 }
